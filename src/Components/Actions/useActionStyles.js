b68b695aa089e5478eef5e77e6600aa9a2a53d89
DIVINE AKACHUKWU <dakachukwu@wazobia.tech>
2023-05-25 15:41:51 +0100

    pulled from HeroBenefits

--- src/Components/Actions/useActionStyles.js
+++ src/Components/Actions/useActionStyles.js
@@ -2,23 +2,23 @@ import { createUseStyles, ThemeProvider, useTheme } from "react-jss";
 
 export const styles = createUseStyles((theme) => ({
   actionMainWrapper: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '96%',
+    display: "flex",
+    justifyContent: "space-between",
+    width: "96%",
   },
   actionTextParagraph: {
     margin: 0,
-    cursor: 'pointer',
+    cursor: "pointer",
   },
   actiontextWrapper: {
-    fontFamily: theme.font.firstfont,
+    fontFamily: theme.font.Aeonik,
     fontWeight: theme.Weight.light,
-    fontSize: theme.size.secondSize,
-    textDecorationLine: 'underline',
+    fontSize: theme.size.defaultSize,
+    textDecorationLine: "underline",
     color: theme.color.lightGrey,
   },
   actionsBtnWrapper: {
-    display: 'flex',
+    display: "flex",
     gap: 16,
   },
 }));
