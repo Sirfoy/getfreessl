eb682c100435d67a786555770927ae451bc91a66
DIVINE AKACHUKWU <dakachukwu@wazobia.tech>
2023-05-26 13:45:51 +0100

    made changes

--- src/Components/Hero/useHeroStyles.js
+++ src/Components/Hero/useHeroStyles.js
@@ -2,7 +2,7 @@ import { createUseStyles, ThemeProvider, useTheme } from "react-jss";
 export const styles = createUseStyles((theme) => ({
   HeroWrapper: {
     height: 413,
-    margin: " 80px  361px 94px 361px",
+    margin: '80px 290px 94px 291px',
   },
   hupperWrapper: {
     height: "416px",
