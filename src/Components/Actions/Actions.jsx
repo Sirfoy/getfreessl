f06ab2256aae3df030a62dff30e053e6b8a30845
Fambegbe Olamileke <ofambegbe@wazobia.tech>
2023-06-23 23:50:10 +0100

    feat: added error handler + polling

--- src/Components/Actions/Actions.jsx
+++ src/Components/Actions/Actions.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import { styles } from "./useActionStyles";
-import { RestartBtn } from "../RestartBtn/RestartBtn";
 import { ProceedBtn } from "../ProceedBtn/ProceedBtn";
-export const Actions = ({ text, onRestartClick, onProceedClick }) => {
+
+export const Actions = ({ text, onProceedClick }) => {
   const classes = styles();
   return (
-      <div className={classes.actionMainWrapper}>
-        <div className={classes.actiontextWrapper}>
-         {text}
-        </div>
-        <div className={classes.actionsBtnWrapper}>
-          <RestartBtn onClick={onRestartClick} />
-          <ProceedBtn onClick={onProceedClick} />
-        </div>
+    <div className={classes.actionMainWrapper}>
+      <div className={classes.actiontextWrapper}>{text}</div>
+      <div className={classes.actionsBtnWrapper}>
+        <ProceedBtn onClick={onProceedClick} />
       </div>
+    </div>
   );
 };
