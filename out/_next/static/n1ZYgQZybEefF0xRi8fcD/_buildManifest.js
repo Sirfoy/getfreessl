db1a0885201254a68456294609ea2b2fe1eb1dc0
divine Akachukwu <sirfoyx@gmail.com>
2024-11-08 21:14:58 +0100

    hew

---
+++ out/_next/static/n1ZYgQZybEefF0xRi8fcD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/932-4134f8ce9f7bdfdc.js","static/chunks/pages/index-c969fb8f9afb7fb9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
